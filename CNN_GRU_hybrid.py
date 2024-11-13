49ff25230ff2a0ab5c1e8e886521c60d95e41aaa
Lim Qi Le <lim.qile@yahoo.com.sg>
2024-11-13 12:51:18 +0800

    update: CNN_GRU model

--- CNN_GRU_hybrid.py
+++ CNN_GRU_hybrid.py
@@ -7,17 +7,17 @@ class CNN_GRU(nn.Module):
 
         self.conv_layers = nn.Sequential(
             nn.Conv1d(in_channels=1, out_channels=64, kernel_size=3),
-            nn.ReLU(),
             nn.BatchNorm1d(num_features=64),
             nn.MaxPool1d(kernel_size=2),
-            nn.Conv1d(64, 128, kernel_size=3),
             nn.ReLU(),
+            nn.Conv1d(64, 128, kernel_size=3),
             nn.BatchNorm1d(num_features=128),
             nn.MaxPool1d(kernel_size=2),
-            nn.Conv1d(128, 256, kernel_size=3),
             nn.ReLU(),
+            nn.Conv1d(128, 256, kernel_size=3),
             nn.BatchNorm1d(num_features=256),
             nn.MaxPool1d(kernel_size=2),
+            nn.ReLU(),
         )
 
         self.gru = nn.GRU(input_size=256, hidden_size=64, batch_first=True)
